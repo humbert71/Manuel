3bcf6dec12d8d3e72b936cad460f58ff47460457
Alumnos <Alumnos@LC01-PC15>
2016-01-14 12:27:57 -0500

    Segundo ejercicio de Poo

--- Emanuel/src/pe/edu/upeu/Poo/clase2/Util.java
+++ Emanuel/src/pe/edu/upeu/Poo/clase2/Util.java
@@ -1,5 +1,31 @@
 package pe.edu.upeu.Poo.clase2;
 
+import java.util.Scanner;
+
 public class Util {
+
+	public static void main(String[] args) {
+
+		Repaso r = new Repaso();
+
+		/**
+		 * Para ingresar datos desde el teclado scan es el objeto
+		 */
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Ingrese su peso en Kg.");
+
+		double peso = scan.nextDouble();
+
+		System.out.println("Ingrese su altura en Metros");
+		double altura = scan.nextDouble();
+
+		System.out.println("Su IMC es: " + r.calcIMC(peso, altura));
+
+	}
+
+
+
 
 }
