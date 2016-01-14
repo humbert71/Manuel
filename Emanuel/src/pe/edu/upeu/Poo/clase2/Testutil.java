320613e97ece0f3d199589ceabfbfd4cc2cb2316
Alumnos <Alumnos@LC01-PC15>
2016-01-14 12:49:56 -0500

    Clase 2 de Poo

---
+++ Emanuel/src/pe/edu/upeu/Poo/clase2/Testutil.java
@@ -0,0 +1,33 @@
+package pe.edu.upeu.Poo.clase2;
+
+import java.util.Scanner;
+
+public class Testutil {
+
+	public static void main(String[] args) {
+
+		Util u = new Util();
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Bienvenido a la calculadora de IMC");
+		System.out.println("Ingrese su nombre:");
+
+		String nombre = scan.nextLine();
+		System.out.println("Hola " + nombre);
+
+		System.out.println("Ingresa tu peso en kilogramos");
+		double peso = scan.nextDouble();
+
+		System.out.println("Ahora ingresa tu altura");
+		double altura = scan.nextDouble();
+
+		System.out.println(nombre + " tu resultado es :" + u.calcIMC(peso, altura));
+
+	}
+
+	
+	
+	
+	
+	
+}
