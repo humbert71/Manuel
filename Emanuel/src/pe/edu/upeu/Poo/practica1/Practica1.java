22da7fdc9c88d94b6983e3f0ba40809f1b7dc871
Alumnos <Alumnos@LC01-PC15>
2016-01-15 12:20:10 -0500

    Practica 1

--- Emanuel/src/pe/edu/upeu/Poo/practica1/Practica1.java
+++ Emanuel/src/pe/edu/upeu/Poo/practica1/Practica1.java
@@ -10,21 +10,21 @@ public class Practica1 {
         
 		@SuppressWarnings("unused")
 		double resultado = 0;
-		System.out.println("ingrese el monto de la venta");
-		double pro = scan.nextDouble();  
-		 if (pro >= 0 && pro <= 1000){
+		System.out.println("Ingrese el monto");
+		double mont = scan.nextDouble();  
+		 if (mont >= 0 && mont <= 1000){
 			 
-			System.out.println("su valor :"+pro+"no tiene bonificacion");
-		 }else if (pro >= 1000 && pro <= 5000){
-			 resultado= pro*3/100;
-		 }else if (pro >= 5000 && pro <= 20000){
-			 resultado=pro*3/100;
-		 }else if (pro > 20000){
-			 resultado=pro*3/100;
+			System.out.println("Su valor :"+mont+"No hay bonificacion");
+		 }else if (mont >= 1000 && mont <= 5000){
+			 resultado= mont*3/100;
+		 }else if (mont >= 5000 && mont <= 20000){
+			 resultado=mont*3/100;
+		 }else if (mont > 20000){
+			 resultado=mont*3/100;
 			 
 			}
 
-		System.out.println("su valor:"+pro+"tiene bonificacion");
+		System.out.println("Su valor:"+mont+"Hay bonificacion");
 
 	
 	
